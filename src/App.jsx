e0f588bedcd59122cfb5bea46c0c51fd0580b735
unknown <hajarelkouni1010@gmail.com>
2025-01-19 19:03:03 +0100

    Version 1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import JobList from "./JobList"; // Import de JobList
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <JobList />
+    </div>
+  );
+};
+
+export default App;
